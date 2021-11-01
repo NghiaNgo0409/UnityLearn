49a482674af8d2457308cf11f42b0d38b1ef5698
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-01 15:21:46 +0700

    Rotate the Driver
    Rotate driver 45 degree to the left

--- Delivery Driver/Assets/Script/Driver.cs
+++ Delivery Driver/Assets/Script/Driver.cs
@@ -7,7 +7,7 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Rotate(0,0,45);
     }
 
     // Update is called once per frame
