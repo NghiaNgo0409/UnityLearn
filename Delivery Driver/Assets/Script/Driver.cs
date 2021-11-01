d44a4acaadbb24704efd00276e12d5aa21966bf9
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-01 15:28:48 +0700

    Rearrange Script
    Add steerSpeed and moveSpeed

--- Delivery Driver/Assets/Script/Driver.cs
+++ Delivery Driver/Assets/Script/Driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = 0.3f;
+    float moveSpeed = 0.03f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,0.3f);
-        transform.Translate(0,0.03f,0);
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0,moveSpeed,0);
     }
 }
