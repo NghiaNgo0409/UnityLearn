0c283b69d71eea64d9c318f8d1b09ac3f676e951
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-03 10:45:12 +0700

    Add Boost Slow mechanic

--- Delivery Driver/Assets/Script/Driver.cs
+++ Delivery Driver/Assets/Script/Driver.cs
@@ -6,6 +6,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField]float steerSpeed = 0.3f;
     [SerializeField]float moveSpeed = 0.03f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float slowSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }    
     }
 }
