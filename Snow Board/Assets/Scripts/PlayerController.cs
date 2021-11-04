979f5579b652602b56c64ae04101d3e91b743968
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-04 16:52:00 +0700

    Add Mechanic for Controlling Character
    Add method AddTorque by using left or right arrow to spin the character

--- Snow Board/Assets/Scripts/PlayerController.cs
+++ Snow Board/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    [SerializeField] float torqueAmount = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torqueAmount);
+        }
     }
 }
