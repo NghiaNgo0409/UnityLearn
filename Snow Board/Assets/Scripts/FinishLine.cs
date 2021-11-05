c380e1dfaeb7c8abd4f69bfdbccb0a7dd03303fa
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-05 10:40:37 +0700

    Write Code to detect player when him finish

--- Snow Board/Assets/Scripts/FinishLine.cs
+++ Snow Board/Assets/Scripts/FinishLine.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.tag == "Player")
+        {
+            Debug.Log("You finished!");
+        }    
     }
 }
