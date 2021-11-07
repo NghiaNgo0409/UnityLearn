c3f372dec39e4549d52bd77e58588316daa2ec65
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-07 14:53:34 +0700

    Coding for DustParticles

--- Snow Board/Assets/Scripts/DustTrails.cs
+++ Snow Board/Assets/Scripts/DustTrails.cs
@@ -6,5 +6,12 @@ public class DustTrails : MonoBehaviour
 {
     [SerializeField] ParticleSystem dustParticles;
 
-    
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        dustParticles.Play();    
+    }
+    void OnCollisionExit2D(Collision2D other) 
+    {
+        dustParticles.Stop();    
+    }
 }
