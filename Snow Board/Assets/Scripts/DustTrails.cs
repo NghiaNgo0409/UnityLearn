98108fa264ca3ead71180ced4373a8c30f248a07
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-07 14:52:43 +0700

    Create variables for dustParticles

--- Snow Board/Assets/Scripts/DustTrails.cs
+++ Snow Board/Assets/Scripts/DustTrails.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class DustTrails : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] ParticleSystem dustParticles;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
