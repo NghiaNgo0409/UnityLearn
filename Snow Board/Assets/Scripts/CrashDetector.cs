5b04f65a827b4c493f0e003c9331a4c6481b4740
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-08 14:48:40 +0700

    Create AudioClip variables and write code to play it

--- Snow Board/Assets/Scripts/CrashDetector.cs
+++ Snow Board/Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,13 @@ public class CrashDetector : MonoBehaviour
 {
     float delayTime = 1.0f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", delayTime);
         }    
     }
