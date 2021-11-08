35aa9684d11df397969964df9f7876fe7040f1c6
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-08 14:44:48 +0700

    Write code to play FinishSFX and config in Unity
    Turn off Play on Awake of FinishSFX

--- Snow Board/Assets/Scripts/FinishLine.cs
+++ Snow Board/Assets/Scripts/FinishLine.cs
@@ -12,6 +12,7 @@ public class FinishLine : MonoBehaviour
         if(other.tag == "Player")
         {
             finishEffect.Play();
+            GetComponent<AudioSource>().Play();
             Invoke("ReloadScene", delayTime);
         }    
     }
