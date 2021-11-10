1e776ba3a2c70e01c2506756590c9cf35c5cd999
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 11:57:14 +0700

    Add Create Asset Menu Attribute

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
     [SerializeField]string question = "Text a question here";
