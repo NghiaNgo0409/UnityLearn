b5adac0997dd9210c4ecb92617e7ab3c170dc2b6
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 11:59:03 +0700

    Add Text Area Attribute

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
+    [TextArea(minLines: 2, maxLines: 6)]
     [SerializeField]string question = "Text a question here";
 
     public string GetQuestion()
