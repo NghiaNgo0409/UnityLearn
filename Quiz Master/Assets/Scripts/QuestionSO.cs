041a24a7331e9b140f324393d61452a6b755833e
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 12:20:01 +0700

    Create a variable to contain correct answer index

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -8,6 +8,7 @@ public class QuestionSO : ScriptableObject
     [TextArea(minLines: 2, maxLines: 6)]
     [SerializeField]string question = "Text a question here";
     [SerializeField]string[] answer = new string[4];
+    [SerializeField]int correctAnswerIndex;
     public string GetQuestion()
     {
         return question;
