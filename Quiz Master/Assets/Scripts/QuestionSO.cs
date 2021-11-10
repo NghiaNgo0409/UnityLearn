76593c53e8c92c9899882195d3eb48fef9bf4c1c
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 11:54:47 +0700

    Change Inheritance
    From MonoBehaviour to SriptableObject

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionSO : MonoBehaviour
+public class QuestionSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
