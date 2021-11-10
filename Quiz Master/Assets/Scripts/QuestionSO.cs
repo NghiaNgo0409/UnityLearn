c7044c868069264de8f77b1a5132099e1652525e
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 12:21:13 +0700

    Create Getter for correct answer index

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -18,4 +18,9 @@ public class QuestionSO : ScriptableObject
     {
         return answer[index];
     }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
