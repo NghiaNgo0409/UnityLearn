a5ac4feadc52110ad899938d2fc44d16627a8efd
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-10 12:19:26 +0700

    Create an array contains answers

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -7,7 +7,7 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(minLines: 2, maxLines: 6)]
     [SerializeField]string question = "Text a question here";
-
+    [SerializeField]string[] answer = new string[4];
     public string GetQuestion()
     {
         return question;
