71eae71eb5eb8145c29ecb3b59f00c122a24c219
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-11 10:58:49 +0700

    Code a command to show the question to the screen

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -10,6 +10,6 @@ public class Quiz : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        questionText.text = question.GetQuestion();
     }
 }
