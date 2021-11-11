8de805ccc415117aec4fbbd858a6c71582c7572e
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-11 10:58:06 +0700

    Create TextMeshProUGUI variable
    To point to question text object on canvas

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class Quiz : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     // Start is called before the first frame update
     void Start()
