09949ec6bc05f867ff5cccc4596d1036136241fb
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-11 10:57:00 +0700

    Create a QuestionSO variable
    To contain a reference of question Scriptable Object

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class Quiz : MonoBehaviour
 {
+    [SerializeField] QuestionSO question;
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
