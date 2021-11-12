3e14191baa8c7c838c87ca6a982e9fdf1655c702
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-12 10:33:37 +0700

    Write UpdateAnswer Method
    This method will reduce the amount of TimeValue each frame. Then it will check the state of the scene is answering or showing answer. Based on that to determine the fill amount of timer image. Finally, if the timer runs out, this method will change the state

--- Quiz Master/Assets/Scripts/Timer.cs
+++ Quiz Master/Assets/Scripts/Timer.cs
@@ -18,4 +18,34 @@ public class Timer : MonoBehaviour
     {
         
     }
+
+    void UpdateAnswer()
+    {
+        timeValue -= Time.deltaTime;
+        if(isAnsweringQuestion)
+        {
+            if(timeValue > 0)
+            {
+                fillFraction = timeValue/timeToCorrectAnswer;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timeValue = timeToShowAnswer;
+            }
+        }
+        else
+        {
+            if(timeValue > 0)
+            {
+                fillFraction = timeValue/timeToShowAnswer;
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timeValue = timeToCorrectAnswer;
+                loadNextQuestion = true;
+            }
+        }
+    }
 }
