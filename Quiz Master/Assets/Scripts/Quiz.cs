fb522c5b95e9c1cf2b04af8f36c9ae462024a399
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-12 18:34:26 +0700

    Update GetNewQuestion
    Whenever new question is displayed, add 1 to question seen variable

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -62,6 +62,7 @@ public class Quiz : MonoBehaviour
             SetButtonState(true);
             SetDefaultButtonSprite();
             GetRandomQuestion();
+            score.IncrementQuestionSeen();
             DisplayQuestion();
         }
     }
