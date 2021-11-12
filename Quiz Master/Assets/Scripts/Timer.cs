064a21b2a6bfaf48fd20e80de1fbcda412b5af1b
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-12 10:25:22 +0700

    Create two variables
    1 for contain the time amount for correcting answer
    1 for contain the time amount for showing answer

--- Quiz Master/Assets/Scripts/Timer.cs
+++ Quiz Master/Assets/Scripts/Timer.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float timeToCorrectAnswer;
+    [SerializeField] float timeToShowAnswer;
 
     // Update is called once per frame
     void Update()
