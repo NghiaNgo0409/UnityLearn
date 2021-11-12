44332eedcff0c8ce15101803fa45d64b68a027d0
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-12 18:27:26 +0700

    Create two new variables
    1 for show correct answers, 1 for showing how many question player has seen

--- Quiz Master/Assets/Scripts/Score.cs
+++ Quiz Master/Assets/Scripts/Score.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    int correctAnswer = 0;
+    int questionSeen = 0;
 }
