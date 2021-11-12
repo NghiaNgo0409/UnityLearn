e9d3bcc02288618e1faf476e8409b4d49ebbf9ac
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-12 19:43:39 +0700

    Change the Start to Awake method

--- Quiz Master/Assets/Scripts/EndScreen.cs
+++ Quiz Master/Assets/Scripts/EndScreen.cs
@@ -8,7 +8,7 @@ public class EndScreen : MonoBehaviour
     [SerializeField] TextMeshProUGUI finalScoreText;
     Score score;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         score = FindObjectOfType<Score>();
     }
