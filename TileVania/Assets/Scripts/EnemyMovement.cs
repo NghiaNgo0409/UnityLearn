801efb5f1ee7ec35970f8b61d834b9970a9c4cd5
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-19 10:55:47 +0700

    Create RigidBody variable and Get Component of it

--- TileVania/Assets/Scripts/EnemyMovement.cs
+++ TileVania/Assets/Scripts/EnemyMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    Rigidbody2D enemyRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
