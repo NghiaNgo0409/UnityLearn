71f317e0df54f0c9b4652527a506640a3a531f11
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-22 21:51:08 +0700

    Create Player variable and Find Object of it

--- TileVania/Assets/Scripts/Bullet.cs
+++ TileVania/Assets/Scripts/Bullet.cs
@@ -7,10 +7,12 @@ public class Bullet : MonoBehaviour
     [SerializeField] float bulletSpeed;
     float xSpeed;
     Rigidbody2D bulletRb;
+    PlayerMovement player;
     // Start is called before the first frame update
     void Start()
     {
         bulletRb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
     }
 
     // Update is called once per frame
