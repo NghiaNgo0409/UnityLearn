47732f3db0160d72cbd8120a25b190418067e3e8
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-22 21:44:47 +0700

    Create Rigidbody variable for Bulllet Scripts

--- TileVania/Assets/Scripts/Bullet.cs
+++ TileVania/Assets/Scripts/Bullet.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Rigidbody2D bulletRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
