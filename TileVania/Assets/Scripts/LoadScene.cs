933d6bfe8427ef7d0f81b8c13423871398aaee43
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-23 15:29:17 +0700

    Create IEnumerator for loading next scene

--- TileVania/Assets/Scripts/LoadScene.cs
+++ TileVania/Assets/Scripts/LoadScene.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator LoadNextScene()
     {
-        
-    }
+        yield return new WaitForSecondsRealtime(1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
