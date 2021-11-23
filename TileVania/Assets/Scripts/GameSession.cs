a6e71db977673360f035870435bc9468d619aa95
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-23 16:20:20 +0700

    Create variable to contain lives of player

--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameSession : MonoBehaviour
 {
+    [SerializeField] int playerLives;
     // Start is called before the first frame update
     void Awake()
     {
