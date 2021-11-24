18426be11a062f7dd0f462cd9f87f649a3f51642
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-24 16:09:00 +0700

    Add Start method

--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives;
@@ -19,6 +19,11 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start() 
+    {
+        
     }
 
     // Update is called once per frame
