678ac5c9b8446922e3fe377e0b129a381376cfee
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-24 16:24:05 +0700

    Create score variable

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int scoreToAdd = 100;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
