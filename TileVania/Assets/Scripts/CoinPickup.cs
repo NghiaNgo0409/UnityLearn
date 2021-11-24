83fd85cee3d0f65de8b78402ef583ed911a4e826
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-24 15:36:00 +0700

    Write new line code
    This mechanic will destroy coin when player picks it up

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other) 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(gameObject);
+        }    
     }
 }
