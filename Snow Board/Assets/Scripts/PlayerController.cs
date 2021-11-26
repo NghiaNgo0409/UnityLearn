1ceb6a109ed80b114efbbce5c2af874dc3ae3012
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-26 22:48:26 +0700

    Update PlayerController.cs

--- Snow Board/Assets/Scripts/PlayerController.cs
+++ Snow Board/Assets/Scripts/PlayerController.cs
@@ -11,14 +11,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float boostSpeed = 30f;
     [SerializeField] float baseSpeed = 20f;
     bool canMove = true;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         surface2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(canMove)
