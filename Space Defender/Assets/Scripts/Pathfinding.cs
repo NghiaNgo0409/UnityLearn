2bafe7c04cca25709d5a263096ae892ee4fbfde8
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-30 10:28:23 +0700

    Create two new variables
    One list for containing waypoint, one for containing the data of path

--- Space Defender/Assets/Scripts/Pathfinding.cs
+++ Space Defender/Assets/Scripts/Pathfinding.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
+    [SerializeField] WayConfigSO wayConfig;
+    List<Transform> waypoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypoints = wayConfig.GetWayPoints();
     }
 
     // Update is called once per frame
