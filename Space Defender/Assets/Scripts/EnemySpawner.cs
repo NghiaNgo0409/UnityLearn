5c55180f89e50d08cda50a283d860e357524c0f4
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-11-30 11:12:04 +0700

    Write code for Spawning Enemy

--- Space Defender/Assets/Scripts/EnemySpawner.cs
+++ Space Defender/Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] WayConfigSO currentWave;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnEnemy()
+    {
+        for(int i = 0; i < currentWave.GetEnemyCount(); i++)
+        {
+            Instantiate(currentWave.GetEnemy(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
+        }
     }
 }
