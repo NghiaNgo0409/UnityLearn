7403f7868f487466ad318626427c9e040424852c
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-01 16:03:13 +0700

    Create damage variable and getter for it

--- Space Defender/Assets/Scripts/Damage Dealer.cs
+++ Space Defender/Assets/Scripts/Damage Dealer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int damage = 10;
+    
 
-    // Update is called once per frame
-    void Update()
+    public int GetDamage()
     {
-        
+        return damage;
     }
 }
