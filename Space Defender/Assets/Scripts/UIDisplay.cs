afece86bedcd67a1a5ee5af24e0cc4a27d3e2182
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-05 12:15:10 +0700

    Create variable for display UI

--- Space Defender/Assets/Scripts/UIDisplay.cs
+++ Space Defender/Assets/Scripts/UIDisplay.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIDisplay : MonoBehaviour
 {
+    [Header("Health")]
+    [SerializeField] Slider healthBar;
+    [SerializeField] Health playerHealth;
+    [Header("Score")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
