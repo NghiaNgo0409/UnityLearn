5882f59cfbc9c72e9dd6fd4fd206d98756de05f6
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-06 12:37:27 +0700

    Write singleton method for score keeper

--- Space Defender/Assets/Scripts/ScoreKeeper.cs
+++ Space Defender/Assets/Scripts/ScoreKeeper.cs
@@ -6,6 +6,22 @@ public class ScoreKeeper : MonoBehaviour
 {
     int score;
 
+    static ScoreKeeper instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public int GetScore()
     {
         return score;
