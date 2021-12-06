afa0e986cac650b95456b2142ddda27568ec4d46
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-06 12:35:16 +0700

    Update UIGameOver.cs

--- Space Defender/Assets/Scripts/UIGameOver.cs
+++ Space Defender/Assets/Scripts/UIGameOver.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIGameOver : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "You scored: " + scoreKeeper.GetScore();
     }
 }
