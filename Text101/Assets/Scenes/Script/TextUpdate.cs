40c8cafa134391c0ecce737ce01a06bf31ad03c3
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-08 12:09:56 +0700

    Create text variable

--- Text101/Assets/Scenes/Script/TextUpdate.cs
+++ Text101/Assets/Scenes/Script/TextUpdate.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextUpdate : MonoBehaviour
 {
+    [SerializeField] Text text;
     // Start is called before the first frame update
     void Start()
     {
