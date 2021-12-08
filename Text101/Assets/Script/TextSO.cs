21a439c12dd37f411a96b52a0563daa3f59f1188
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-08 12:45:40 +0700

    Update TextSO.cs

--- Text101/Assets/Script/TextSO.cs
+++ Text101/Assets/Script/TextSO.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class TextSO : ScriptableObject
 {
     [TextArea(10, 14)][SerializeField] string text;
+    [SerializeField] TextSO[] nextState;
 
     public string GetText()
     {
         return text;
+    }
+
+    public TextSO[] GetNextStates()
+    {
+        return nextState;
     }
 }
