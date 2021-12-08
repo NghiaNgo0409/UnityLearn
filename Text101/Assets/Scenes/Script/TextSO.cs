209388eed6d25d6a6234b9340200300ff20f6311
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-08 12:14:16 +0700

    Update TextSO.cs

--- Text101/Assets/Scenes/Script/TextSO.cs
+++ Text101/Assets/Scenes/Script/TextSO.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(menuName = "textSave")]
 public class TextSO : ScriptableObject
 {
-    [SerializeField] string text;
+    [TextArea(10, 14)][SerializeField] string text;
 }
