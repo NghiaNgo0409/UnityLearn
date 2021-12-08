5da478ba6f502ae69577405f3374b1435365b461
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-08 12:22:30 +0700

    Create getter for SO

--- Text101/Assets/Script/TextSO.cs
+++ Text101/Assets/Script/TextSO.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class TextSO : ScriptableObject
 {
     [TextArea(10, 14)][SerializeField] string text;
+
+    public string GetText()
+    {
+        return text;
+    }
 }
