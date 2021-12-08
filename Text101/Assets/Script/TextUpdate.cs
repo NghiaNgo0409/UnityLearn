b091425c56ed13e615e53561872387909e0be31a
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-08 12:23:03 +0700

    Create new TextSO variables

--- Text101/Assets/Script/TextUpdate.cs
+++ Text101/Assets/Script/TextUpdate.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class TextUpdate : MonoBehaviour
 {
     [SerializeField] Text text;
+    [SerializeField] TextSO startingState;
+    TextSO currentState;
     // Start is called before the first frame update
     void Start()
     {
