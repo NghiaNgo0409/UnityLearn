dea4e4e3958e9d7bdd139cc9a9f57ef42a27dd77
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-09 11:58:49 +0700

    Write code to change state

--- Text101/Assets/Script/TextUpdate.cs
+++ Text101/Assets/Script/TextUpdate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,35 @@ public class TextUpdate : MonoBehaviour
     [SerializeField] Text text;
     [SerializeField] TextSO startingState;
     TextSO currentState;
+    TextSO[] nextStates;
     // Start is called before the first frame update
     void Start()
     {
         currentState = startingState;
         text.text = currentState.GetText();
+        nextStates = startingState.GetNextStates();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = nextStates[1];
+        }
+        else
+        {
+            currentState = startingState;
+        }
+        text.text = currentState.GetText();
     }
 }
