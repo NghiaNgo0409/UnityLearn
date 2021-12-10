9d41d3d4da0562e7a868fa079954f175ac2204be
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-10 22:35:01 +0700

    Create variable
    To contain the build index of the current scene

--- Number Wizard/Assets/Scripts/LoadScene.cs
+++ Number Wizard/Assets/Scripts/LoadScene.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int currentSceen;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        currentSceen = SceneManager.GetActiveScene().buildIndex;
     }
 }
