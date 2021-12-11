aad4c450756ed7fea05851e66f56a1943cbbcd1a
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-11 14:25:11 +0700

    Create new variable
    To contain text guess

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
     [SerializeField] int min;
     [SerializeField] int max;
     int guess;
+
+    [SerializeField] TextMeshProUGUI guessText;
     // Start is called before the first frame update
     void Start()
     {
