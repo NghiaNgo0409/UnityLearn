acd301971e339620413fd7926c70bafe2d35bc93
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-14 16:16:05 +0700

    Create new variables

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    float distancePaddleToBall;
+    [SerializeField] Paddle paddle;
     // Start is called before the first frame update
     void Start()
     {
