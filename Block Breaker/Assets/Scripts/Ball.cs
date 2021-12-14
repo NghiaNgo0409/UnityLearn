f7dafcdc90bbcac7097983e41739cc039c694b49
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-14 16:17:16 +0700

    Change type of variable of distance

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    float distancePaddleToBall;
+    Vector2 distancePaddleToBall;
     [SerializeField] Paddle paddle;
     // Start is called before the first frame update
     void Start()
