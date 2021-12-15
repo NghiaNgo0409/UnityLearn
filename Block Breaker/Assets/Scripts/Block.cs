6b42731e072f2650171df12a2a352c84a54fb235
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-15 11:02:22 +0700

    Destroy block when in collided with others objects

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D other) {
+        Destroy(gameObject);
     }
 }
