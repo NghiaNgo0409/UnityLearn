dc03f73dcc9638b2260dad4c10e3c106174be197
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 10:44:39 +0700

    Create audio variable
    To contain the break block sound

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
     private void OnCollisionEnter2D(Collision2D other) {
         Destroy(gameObject);
     }
