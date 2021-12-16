c127b3b24e147529a5c7c6dc3e7f77db71cc7e01
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 11:26:44 +0700

    Get build index of current scene

--- Block Breaker/Assets/Scripts/LoadScene.cs
+++ Block Breaker/Assets/Scripts/LoadScene.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    int currentSceneIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
