3b4ac6a26441634a7c1dc2969057487a8e67b8c2
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 11:36:34 +0700

    Update loseline

--- Block Breaker/Assets/Scripts/LoseLine.cs
+++ Block Breaker/Assets/Scripts/LoseLine.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class LoseLine : MonoBehaviour
 {
+    LoadScene sceneLoader;
+    private void Start() {
+        sceneLoader = FindObjectOfType<LoadScene>();
+    }
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("You lose!");
+        sceneLoader.LoadNextScene();
     }
 }
