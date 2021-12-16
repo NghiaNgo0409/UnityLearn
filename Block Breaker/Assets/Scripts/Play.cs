db2f2a2045a5cf3b2206233fa0120f0e62620dff
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 11:04:10 +0700

    Update Play.cs

--- Block Breaker/Assets/Scripts/Play.cs
+++ Block Breaker/Assets/Scripts/Play.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Play : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int breakableBlocks;
 
-    // Update is called once per frame
-    void Update()
+    public void CountBreakableBlock()
     {
-        
+        breakableBlocks++;
     }
 }
