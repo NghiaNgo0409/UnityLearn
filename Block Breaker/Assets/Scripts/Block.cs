1f7cddae92aeff383c91856e27ab33ac7331e417
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 11:05:00 +0700

    Find object and get component of play in blocks script

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    Play play;
     [SerializeField] AudioClip breakSound;
+    private void Start() {
+        play = FindObjectOfType<Play>();
+    }
     private void OnCollisionEnter2D(Collision2D other) {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
