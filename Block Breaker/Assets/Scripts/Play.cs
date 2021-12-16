d902012e6c6cbdc7626e4f3017a4f8deee872bca
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-16 11:33:42 +0700

    Update Play.cs

--- Block Breaker/Assets/Scripts/Play.cs
+++ Block Breaker/Assets/Scripts/Play.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Play : MonoBehaviour
 {
     [SerializeField] int breakableBlocks;
+    LoadScene sceneLoader;
+
+    private void Start() {
+        sceneLoader = FindObjectOfType<LoadScene>();
+    }
 
     public void CountBreakableBlock()
     {
         breakableBlocks++;
+    }
+
+    public void DestroyBlocks()
+    {
+        breakableBlocks--;
+        if(breakableBlocks == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
