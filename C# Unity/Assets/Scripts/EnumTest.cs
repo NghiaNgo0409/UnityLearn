0ef6513b0ed3fdc8c5794bf6409f508e02debe31
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2021-12-30 23:57:03 +0700

    Create enum

--- C# Unity/Assets/Scripts/EnumTest.cs
+++ C# Unity/Assets/Scripts/EnumTest.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnumTest : MonoBehaviour
 {
+    enum State {Ready, Playing, GameOver};
     // Start is called before the first frame update
     void Start()
     {
