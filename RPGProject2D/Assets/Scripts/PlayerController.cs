5b9e6d10e502d8bd476be5d8dec01b37b59d80f7
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-02-16 22:13:10 +0700

    Animation player movement

--- RPGProject2D/Assets/Scripts/PlayerController.cs
+++ RPGProject2D/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D playerRb;
+    Rigidbody2D playerRb;
+    Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * 6f;
+
+        playerAnim.SetFloat("speedX", playerRb.velocity.x);
+        playerAnim.SetFloat("speedY", playerRb.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            playerAnim.SetFloat("lastPosX", Input.GetAxisRaw("Horizontal"));
+            playerAnim.SetFloat("lastPosY", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
