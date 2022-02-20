8fdceeb92bb7965c18d385437ecfee3784bc7e6b
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-02-20 11:38:52 +0700

    Bound Camera

--- RPGProject2D/Assets/Scripts/CameraFollow.cs
+++ RPGProject2D/Assets/Scripts/CameraFollow.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] Tilemap themap;
+    Vector3 bottomLeft;
+    Vector3 topRight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bottomLeft = themap.localBounds.min;
+        topRight = themap.localBounds.max;
     }
 
     // Update is called once per frame
@@ -17,5 +22,12 @@ public class CameraFollow : MonoBehaviour
         pos = PlayerController.instance.transform.position;
         pos.z -= 10;
         transform.position = pos;
+
+        LimitCamera();
+    }
+
+    void LimitCamera()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeft.x, topRight.x), Mathf.Clamp(transform.position.y, bottomLeft.y, topRight.y), transform.position.z);
     }
 }
