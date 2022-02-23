81817087cfd0ab97828afb75dc7a2ff7c7bd2678
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-02-23 11:05:28 +0700

    Level Manager

--- RPGProject2D/Assets/Scripts/PlayerStats.cs
+++ RPGProject2D/Assets/Scripts/PlayerStats.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {   
-    [SerializeField] int playerLevel;
+    [SerializeField] int playerLevel = 1;
     [SerializeField] int currentExp;
+    [SerializeField] int maxLevel = 100;
     [SerializeField] int[] expToLevelUp;
     [SerializeField] int baseExp = 1000;
     [SerializeField] int currentHp;
@@ -24,12 +25,31 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        expToLevelUp = new int[maxLevel];
+        expToLevelUp[1] = baseExp;
+
+        for(int i = 2; i < expToLevelUp.Length; i++)
+        {
+            expToLevelUp[i] = Mathf.FloorToInt(expToLevelUp[i-1] * 1.05f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            AddExp(500);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        currentExp += expToAdd;
+        if(currentExp > expToLevelUp[playerLevel])
+        {
+            currentExp -= expToLevelUp[playerLevel];
+            playerLevel++;
+        }
     }
 }
