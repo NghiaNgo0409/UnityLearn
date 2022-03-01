124e26da7f3c51e86e2befca2c6e5319c14640db
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-03-01 23:27:37 +0700

    1

--- Lesson2 - Platform/Assets/Script/PlayerController.cs
+++ Lesson2 - Platform/Assets/Script/PlayerController.cs
@@ -8,9 +8,8 @@ public class PlayerController : MonoBehaviour
     Animator playerAnim;
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
-    bool isJump;
-    bool isFacingRight = false;
-    PlayerState playerState;
+    [SerializeField] SpriteRenderer playerSprite;
+    [SerializeField] PlayerState state;
     
     [Header("Check Ground")]
     [SerializeField] Transform groundCheckPos;
@@ -22,38 +21,23 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody2D>();
         playerAnim = GetComponent<Animator>();
-        playerState = PlayerState.Idle;
+        state = PlayerState.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerState == PlayerState.Attack) return;  
-        if(Input.GetButtonDown("Fire1"))
+        if(state == PlayerState.Attack) return;
+        if(state == PlayerState.Jump)
         {
-            playerState = PlayerState.Attack;
-            playerAnim.SetTrigger("Attack");
+            if(state == PlayerState.Idle)
+            {
+                state = PlayerState.Idle;
+                playerAnim.SetTrigger("Idle");
+            }
             return;
         }
         GetInput();
-        if(Input.GetAxisRaw("Horizontal") > 0 && !isFacingRight)
-        {
-            playerState = PlayerState.Run;
-            Flip();
-        }
-        else if(Input.GetAxisRaw("Horizontal") < 0 && isFacingRight)
-        {
-            playerState = PlayerState.Run;
-            Flip();
-        }
-        else
-        {
-            if(playerState != PlayerState.Idle)
-            {
-                playerState = PlayerState.Idle;
-                playerAnim.SetTrigger("Idle");
-            }
-        }
     }
 
     private void FixedUpdate() {
@@ -63,30 +47,46 @@ public class PlayerController : MonoBehaviour
 
     void GetInput()
     {
-        //Move
-        playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, playerRb.velocity.y);
-        playerAnim.SetFloat("xVelocity", playerRb.velocity.x);
-        //Jump
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKey(KeyCode.RightArrow))
         {
-            isJump = true;
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            playerSprite.flipX = true;
+            playerAnim.SetBool("Run", true);
+            state = PlayerState.Run;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            playerSprite.flipX = false;
+            playerAnim.SetBool("Run", true);
+            state = PlayerState.Run;
+        }
+        else if(Input.GetButtonDown("Fire1"))
+        {
+            state = PlayerState.Attack;
+            playerAnim.SetTrigger("Attack");
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            state = PlayerState.Jump;
+            playerAnim.SetTrigger("Jump");
+        }
+        else
+        {
+            if(state != PlayerState.Idle)
+            {
+                state = PlayerState.Idle;
+                playerAnim.SetTrigger("Idle");
+                playerAnim.SetBool("Run", false);
+            }
         }
     }
-
-    void Flip()
-    {
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-        isFacingRight = !isFacingRight;
-    }
-    
+  
     void Jump()
     {
-        if(isJump && onGround)
+        if(state == PlayerState.Jump && onGround)
         {
             playerRb.velocity += new Vector2(0f, jumpForce * Time.deltaTime);
-            isJump = false;
         }
     }
 
@@ -99,7 +99,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             onGround = false;
-            isJump = false;
         }
     }
 
@@ -110,7 +109,7 @@ public class PlayerController : MonoBehaviour
 
     public void Attack01_End()
     {
-        playerState = PlayerState.Idle;
+        state = PlayerState.Idle;
         playerAnim.SetTrigger("Idle");
     }
 }
