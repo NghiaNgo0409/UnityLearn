aa32cfac72a8be08066f6bff2407f838b6a564ef
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-03-05 12:40:45 +0700

    Enemy Hit

---
+++ Lesson2 - Platform/Assets/Script/DamageObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            EnemyController.instance.DecreaseHP();
+        }
+    }
+}
--- Lesson2 - Platform/Assets/Script/EnemyController.cs
+++ Lesson2 - Platform/Assets/Script/EnemyController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public static EnemyController instance;
+    [SerializeField] int hp = 3;
+    public bool isAttacked;
+    private void Awake() {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DecreaseHP()
+    {
+        hp--;
+        isAttacked = false;
     }
 }
