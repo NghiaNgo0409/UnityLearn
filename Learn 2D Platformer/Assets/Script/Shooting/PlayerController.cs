2fa8f533acf6b8f44801efcc759e247c35995448
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-04-11 16:56:19 +0700

    Player Shooting Run

--- Learn 2D Platformer/Assets/Script/Shooting/PlayerController.cs
+++ Learn 2D Platformer/Assets/Script/Shooting/PlayerController.cs
@@ -6,16 +6,45 @@ namespace Shooting
 {
     public class PlayerController : MonoBehaviour
     {
+        Rigidbody2D playerRb;
+        Animator playerAnim;
+        
+        float horizontalInput;
+
+        [SerializeField] float moveSpeed;
         // Start is called before the first frame update
         void Start()
         {
-            
+            playerRb = GetComponent<Rigidbody2D>();
+            playerAnim = GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            GetInput();
+            UpdateAnimation();
+        }
+
+        void FixedUpdate() 
+        {
+            Move();
+        }
+
+        void GetInput()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+        }
+
+        void Move()
+        {
+            float moveValue = horizontalInput * moveSpeed * Time.fixedDeltaTime;
+            playerRb.velocity = new Vector2(moveValue, playerRb.velocity.y);
+        }
+
+        void UpdateAnimation()
+        {
+            playerAnim.SetFloat("xVelocity", Mathf.Abs(horizontalInput));
         }
     }
 
