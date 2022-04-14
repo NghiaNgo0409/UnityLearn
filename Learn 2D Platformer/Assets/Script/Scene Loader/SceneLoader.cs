fc46d68ddb0045fa59ffac63d11f0041c741582e
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-04-14 10:25:23 +0700

    Scene Loader with Loading Bar

---
+++ Learn 2D Platformer/Assets/Script/Scene Loader/SceneLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider loadingSlider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingSlider.value = progress;
+            
+
+            yield return null;
+        }
+    }
+}
