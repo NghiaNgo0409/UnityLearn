476268ec50dc1cf7201d75dce85bb1bc0c271751
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-04-20 15:37:51 +0700

    Code for parallax

--- Learn 2D Platformer/Assets/Script/Parallax/ParallaxBG.cs
+++ Learn 2D Platformer/Assets/Script/Parallax/ParallaxBG.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class ParallaxBG : MonoBehaviour
 {
+    [SerializeField] Transform[] backgrounds;
+    float[] parallaxScales;
+    float smoothing = 1f;
+
+    Transform cam;
+    Vector3 previousCamPos;
+
+    private void Awake() {
+        cam = Camera.main.transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        previousCamPos = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * (-1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
     }
 }
